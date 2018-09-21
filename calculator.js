77354558315f3932a1a6fe3fe05ded45a97c1ce5
Mark Bailey <markbailey0356@gmail.com>
2018-09-21 17:32:21 +0930

    Add curried operate function

--- calculator.js
+++ calculator.js
@@ -1,4 +1,24 @@
+// functional programming functions
+// curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c
+// courtesy of the mostly-adequate-guide
+const curry = (fn) => {
+  const arity = fn.length;
+
+  return function $curry(...args) {
+    if (args.length < arity) {
+      return $curry.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args);
+  };
+};
+
+// calculator functions
 const add = (x,y) => x + y;
 const subtract = (x,y) => x - y;
 const multiply = (x,y) => x * y;
 const divide = (x,y) => x / y;
+
+// curried operate function
+const operate = curry((operator, x, y) => operator(x,y));
+
