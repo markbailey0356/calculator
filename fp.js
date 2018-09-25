2566e7390267f084a212a4127510b697eb797da2
Mark Bailey <markbailey0356@gmail.com>
2018-09-25 16:33:24 +0930

    Move FP functions to separate file

---
+++ fp.js
@@ -0,0 +1,14 @@
+// functional programming functions
+// curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c
+// courtesy of the mostly-adequate-guide
+const curry = (fn) => {
+  const arity = fn.length;
+
+  return function $curry(...args) {
+    if (args.length < arity) {
+      return $curry.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args);
+  };
+};
